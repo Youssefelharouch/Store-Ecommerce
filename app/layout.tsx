fc912d1922d8b85cb037fb13574fea07b60b4b67
Youssef Elharouch <elharouchyoussef1998@gmail.com>
2023-08-21 18:47:54 +0100

    commit store

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,9 @@ import Footer from '@/components/footer'
 import './globals.css'
 import type { Metadata } from 'next'
 import { Urbanist } from 'next/font/google'
+import Navbar from '@/components/Navbar'
+import ToastProvider from '@/providers/toast-provider'
+import ModalProvider from '@/providers/modal-provider'
 
 const font = Urbanist({ subsets: ['latin'] })
 
@@ -18,7 +21,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
-        <Navbar />
+      <ToastProvider />
+      <ModalProvider />
+      <Navbar />
         {children}
         <Footer />
       </body>
